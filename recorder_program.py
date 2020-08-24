ae3654075266f1047298835a345bcba46c2c76e7
araamz <64193794+araamz@users.noreply.github.com>
2020-08-24 02:23:01 -0700

    Fixed Pressure and Temperature
    Fixed Pressure and Temperature values not updating. Changes are listed as follows:
    
    ~ Pressure and Temperature values are initilized on function calls.
    + Added time stamps to log statements.

--- recorder_program.py
+++ recorder_program.py
@@ -3,23 +3,22 @@ import smbus2
 import datetime
 from time import sleep
 
-print("PROGRAM START -> PROGRAM LOOP")
+print("PROGRAM START AT " + datetime.datetime.now() +  " -> PROGRAM LOOP")
 
 class bme280_IC:
 
     def __init__(self, device_port, device_address):
-        self.device_port = device_port
-        self.device_address = device_address
         self.device_bus = smbus2.SMBus(device_port)
         bme280.load_calibration_params(self.device_bus, self.device_address)
-        self.device_data_link = bme280.sample(self.device_bus, self.device_address)
     
     @property
     def temperature_value(self):
+        self.device_data_link = bme280.sample(self.device_bus, self.device_address)
         return self.device_data_link.temperature
 
     @property
     def pressure_value(self):
+        self.device_data_link = bme280.sample(self.device_bus, self.device_address)
         return self.device_data_link.pressure
 
 main_sensor = bme280_IC(1, 0x76)
@@ -30,13 +29,13 @@ current_file.close()
 
 while True: 
 
-    print("START -> MAIN EVENT LOOP")
+    print("START AT " + datetime.datetime.now() + " -> MAIN EVENT LOOP")
 
     current_date = datetime.datetime.today().strftime('%d_%m_%Y')
 
     if (current_date == datetime.datetime.today().strftime('%d_%m_%Y')):
 
-        print("IF -> MAIN EVENT LOOP")
+        print("(IF) " + datetime.datetime.now() + " -> MAIN EVENT LOOP")
 
         current_file = open(datetime.datetime.today().strftime('DATA/%d_%m_%Y') + '.txt', "a+", 1)
         current_file.write(datetime.datetime.now().strftime('%H_%M') + ":" + str(main_sensor.temperature_value) + "_" + str(main_sensor.pressure_value) + "\n")
@@ -44,7 +43,7 @@ while True:
 
     else: 
 
-        print("ELSE -> MAIN EVENT LOOP")
+        print("(ELSE) AT " + datetime.datetime.now() " -> MAIN EVENT LOOP")
 
         current_file.close()
         current_file = open(datetime.datetime.today().strftime('DATA/%d_%m_%Y'), "a+", 1)
